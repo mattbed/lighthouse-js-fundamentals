710fa55320c800dc0e4c865d1fba1e1f7d66daf7
Matthew <m.bednarski@gmail.com>
2021-12-26 17:09:44 +0000

    Assignment to calculate how many units of 100 can be made out of a given input

---
+++ howManyHundreds.js
@@ -0,0 +1,10 @@
+function howManyHundreds(num) {
+  let containers = (num / 100) - (num % 100 / 100);
+  return containers;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
