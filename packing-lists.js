d03e9cfaa348147e727c0e7de35d221a9650b1e3
Matthew <m.bednarski@gmail.com>
2021-12-27 15:19:21 +0000

    added an example of a while loop to cycle through items in an array

--- packing-lists.js
+++ packing-lists.js
@@ -1,6 +1,15 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-console.log("Kitchen stuff to pack:");
+// first method using for loop
+/*console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}*/
+
+//second method using while loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
