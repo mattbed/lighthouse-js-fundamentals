84f32b4a861e30be3900d4a7103332f88b2bbabb
Matthew <m.bednarski@gmail.com>
2021-12-27 16:42:05 +0000

    Assignment to print the final instance of an element from an array input

---
+++ looping-lastIndexOf.js
@@ -0,0 +1,16 @@
+function lastIndexOf(array, element) {
+  let output = -1
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) {
+      output = i
+    }
+  }
+  return output;
+}
+
+//test examples
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
